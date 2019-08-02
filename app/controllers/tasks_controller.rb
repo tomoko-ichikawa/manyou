3459a968a96b05184317775054f8f74b9e402b37
tomokoichikawa <takator@yahoo.co.jp>
2019-08-02 22:05:28 +0900

    編集作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,19 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path,notice:"taskを編集しました"
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def task_params
