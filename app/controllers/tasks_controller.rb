067e91612d3ea074cde0a1aa9a2b50a40157546a
tomokoichikawa <takator@yahoo.co.jp>
2019-08-02 22:10:00 +0900

    共通化

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only:[:show, :edit, :update]
+
   def index
     @tasks = Task.all
   end
@@ -33,5 +35,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:task_name,:deadline,:priority,:status,:content)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
