5d257e99f697c90f8808cf3dd4a07b89d3b902c1
tomokoichikawa <takator@yahoo.co.jp>
2019-08-02 22:19:51 +0900

    フィードバックメッセージ表示

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,8 +10,12 @@ class TasksController < ApplicationController
   end
 
   def create
-    Task.create(task_params)
-    redirect_to new_task_path
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "taskを作成しました"
+    else
+      render 'new'
+    end
   end
 
   def show
