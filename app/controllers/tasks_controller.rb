f3c6fed90a86e82ff803b3ca604f66e30dfab688
tomokoichikawa <takator@yahoo.co.jp>
2019-08-02 22:15:23 +0900

    削除作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only:[:show, :edit, :update]
+  before_action :set_task, only:[:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
@@ -29,6 +29,11 @@ class TasksController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice:"taskを削除しました"
   end
 
   private
