68e747c640329f9672ed02f0579e7a03a17430c5
tomokoichikawa <takator@yahoo.co.jp>
2019-08-02 18:56:26 +0900

    taskモデル作成

---
+++ db/migrate/20190802092255_create_tasks.rb
@@ -0,0 +1,14 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.integer :user_id
+      t.integer :label_id
+      t.string :task_name
+      t.datetime :deadline
+      t.integer :priority
+      t.string :status
+      t.text :content
+      t.timestamps
+    end
+  end
+end
