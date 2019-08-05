21198514ab32624b2a7cdc1a282632d0a7011a2d
tomokoichikawa <takator@yahoo.co.jp>
2019-08-05 14:58:00 +0900

    Add:Rspec

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+
+  it "task_nameが空ならバリデーションが通らない" do
+    task = Task.new(task_name: '', content: '失敗テスト')
+    expect(task).not_to be_valid
+  end
+
+  it "contentが空ならバリデーションが通らない" do
+    task = Task.new(task_name: '失敗しないテスト', note: '失敗テスト')
+    expect(task).to be_valid
+  end
+
+  it "titleとcontentに内容が記載されていればバリデーションが通る" do
+    task = Task.new(task_name: '失敗しないテスト', note: '失敗しないテスト')
+    expect(task).to be_valid
+  end
+end
