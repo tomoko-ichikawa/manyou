eb4cddb16b863ca98521ae1a710166c460a83544
tomokoichikawa <takator@yahoo.co.jp>
2019-08-05 10:13:26 +0900

    Add:Rspec

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -10,38 +10,38 @@ RSpec.feature "タスク管理機能", type: :feature do
       # backgroundの中に記載された記述は、そのカテゴリ内（feature "タスク管理機能", type: :feature do から endまでの内部）
       # に存在する全ての処理内（scenario内）で実行される
       # （「タスク一覧のテスト」でも「タスクが作成日時の降順に並んでいるかのテスト」でも、background内のコードが実行される）
-      Task.create!(task_name: 'test_task_01', content: 'testtesttest')
-      Task.create!(task_name: 'test_task_02', content: 'mofmofmofmof')
+      FactoryBot.create(:task)
+      FactoryBot.create(:second_task)
   end
 
+
   scenario "タスク一覧のテスト" do
+# あらかじめタスク一覧のテストで使用するためのタスクを二つ作成す
+
     # tasks_pathにvisitする（タスク一覧ページに遷移する）
     visit tasks_path
     save_and_open_page
     # visitした（到着した）expect(page)に（タスク一覧ページに）「testtesttest」「samplesample」という文字列が
     # have_contentされているか？（含まれているか？）ということをexpectする（確認・期待する）テストを書いている
-    expect(page).to have_content 'testtesttest'
-    expect(page).to have_content 'samplesample'
+    expect(page).to have_content 'Factoryで作ったデフォルトのタイトル１'
+    expect(page).to have_content 'Factoryで作ったデフォルトのコンテント１'
 
   end
 
   scenario "タスク作成のテスト" do
     visit new_task_path
 
-    fill_in 'task[task_name]', with: 'testtesttest'
-    fill_in 'task[content]', with: 'samplesample'
-    click_on 'Create Task'
-    expect(page).to have_content 'testtesttest'
-    expect(page).to have_content 'samplesample'
+    fill_in 'task[task_name]', with: 'Factoryで作ったデフォルトのタイトル１'
+    fill_in 'task[content]', with: 'Factoryで作ったデフォルトのコンテント１'
+    click_on '登録する'
+    expect(page).to have_content 'Factoryで作ったデフォルトのタイトル１'
+    expect(page).to have_content 'Factoryで作ったデフォルトのコンテント１'
   end
 
   scenario "タスク詳細のテスト" do
-    # backgroundに必要なタスクデータの作成処理が書かれているので、ここで書く必要がない
-
-    # タスクが作成日時の降順に並んでいるかのテスト
   end
 
   scenario "タスクが作成日時の降順に並んでいるかのテスト" do
-
+    # ここにテスト内容を記載する
   end
 end
