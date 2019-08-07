76c8bfa3f79aa3d2a82c430fc3c51f1d8552ef9f
tomokoichikawa <takator@yahoo.co.jp>
2019-08-07 15:00:24 +0900

    Add:検索

---
+++ db/migrate/20190807052001_add_index_tasks_taskname.rb
@@ -0,0 +1,5 @@
+class AddIndexTasksTaskname < ActiveRecord::Migration[5.2]
+  def change
+      add_index :tasks, :task_name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_143947) do
+ActiveRecord::Schema.define(version: 2019_08_07_052001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_08_06_143947) do
     t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["task_name"], name: "index_tasks_on_task_name"
   end
 
 end
