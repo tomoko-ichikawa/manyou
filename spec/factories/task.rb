2fd5393f28fcdd6b1d030c6418d3b14504d6f3dd
tomokoichikawa <takator@yahoo.co.jp>
2019-08-07 16:25:25 +0900

    Fix:Rspec

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -2,12 +2,10 @@ FactoryBot.define do
   factory :task do
     task_name { 'Factoryで作ったデフォルトのタイトル１' }
     content { 'Factoryで作ったデフォルトのコンテント１' }
-    deadline {'2019,08.07'}
   end
 
   factory :second_task, class: Task do
     task_name { 'Factoryで作ったデフォルトのタイトル２' }
     content { 'Factoryで作ったデフォルトのコンテント２' }
-    deadline {'2019.10.31'}
   end
 end
