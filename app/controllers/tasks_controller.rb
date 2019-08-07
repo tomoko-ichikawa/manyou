4fa89fe64fdd05a6ca70f118631402efa7eba9c4
tomokoichikawa <takator@yahoo.co.jp>
2019-08-07 21:45:23 +0900

    Add:優先度のソート

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,12 +2,12 @@ class TasksController < ApplicationController
   before_action :set_task, only:[:show, :edit, :update, :destroy]
 
   def index
-    if params[:sort_expired]
-      @tasks = Task.order(deadline: :desc)
+    if params[:sort_priority]
+      @tasks = Task.important
     elsif params[:sort_expired]
-      @tasks = Task.order(deadline: :asc)
+      @tasks = Task.expired
     elsif params[:task] == nil
-      @tasks = Task.order(created_at: :desc)
+      @tasks = Task.latest
     elsif params[:task][:search]
       @tasks = Task.search(params)
     end
