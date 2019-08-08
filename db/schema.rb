cce163aadbdbb4c18a30bad8a4e43bb42aa0991b
tomokoichikawa <takator@yahoo.co.jp>
2019-08-08 14:04:37 +0900

    Add:user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_143947) do
+ActiveRecord::Schema.define(version: 2019_08_08_045735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 2019_08_06_143947) do
     t.integer "priority"
     t.string "status"
     t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_name"], name: "index_tasks_on_task_name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
