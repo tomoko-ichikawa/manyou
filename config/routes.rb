91a913a99b3a8c389ced5179a94fe2183a69e4b8
tomokoichikawa <takator@yahoo.co.jp>
2019-08-08 23:07:51 +0900

    Add:ログアウト

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
-  resources :sessions, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users
 end
