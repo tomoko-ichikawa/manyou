106c32572a0bf5269b5152213c6c681d152ce2f6
tomokoichikawa <takator@yahoo.co.jp>
2019-08-09 16:01:30 +0900

    Add:新規ユーザー登録後、同時にログイン

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+
+
   def new
     @user = User.new
   end
@@ -6,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id unless current_user
       redirect_to user_path(@user.id)
     else
       render 'new'
