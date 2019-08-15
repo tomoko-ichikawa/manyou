f40091ce3c28bcb4294bd486d4a7eb3c8c50d971
tomokoichikawa <takator@yahoo.co.jp>
2019-08-15 23:33:00 +0900

    Fix:Rspec

--- spec/features/admin_user.spec.rb
+++ spec/features/admin_user.spec.rb
@@ -4,32 +4,34 @@ RSpec.feature "ユーザーテスト機能", type: :feature do
 
   background do
   # あらかじめタスク一覧のテストで使用するためのユーザーを作成する
-  @user1=FactoryBot.create(:user)
+  user = FactoryBot.create(:user)
   end
 
   before do
   #ログイン画面から、まずはログイン
     visit new_session_path
-    fill_in 'email', with: 'test@test.com'
-    fill_in 'password', with: 'password'
-    click_button 'ログイン'
+
+    click_link 'ログイン'
+    fill_in 'session[email]', with: 'test@test.com'
+    fill_in 'session[password]', with: 'password'
+
   end
 
   scenario "ユーザー一覧のテスト" do
     visit admin_users_path
 
-    expect(page).to have_content 'DIC'
+    expect(page).to have_content 'test@test.com'
   end
 
   scenario "ユーザー作成のテスト" do
     visit admin_users_path
 
-    click_on '登録'
+    click_link '登録'
 
-    fill_in 'user_name',with:'User'
-    fill_in 'email',with:'test@test.com'
-    fill_in 'password',with:'user'
-    fill_in 'password confirmation',with:'user'
+    fill_in 'user_user_name',with:'User'
+    fill_in 'user_email',with:'user@user.com'
+    fill_in 'user_password',with:'user'
+    fill_in 'user_password_confirmation',with:'user'
 
     expect(page).to have_content 'User'
   end
@@ -37,7 +39,7 @@ RSpec.feature "ユーザーテスト機能", type: :feature do
   scenario "ユーザー詳細のテスト" do
     visit admin_users_path
 
-    click_on '詳細画面'
+    click_link '詳細'
 
     expect(page).to have_content 'DIC'
   end
@@ -45,14 +47,14 @@ RSpec.feature "ユーザーテスト機能", type: :feature do
   scenario "ユーザー更新のテスト" do
     visit admin_users_path
 
-    click_on '編集画面'
+    click_on '編集'
 
     fill_in 'Name',with:'Admin'
     fill_in 'Email',with:'admin@user.com'
     fill_in 'Password',with:'admin'
-    fill_in 'Password confirmation',with:'admin'
+    fill_in 'Password_confirmation',with:'admin'
 
-    click_on '更新する'
+    click_on '更新'
 
     expect(page).to have_content 'Admin'
   end
