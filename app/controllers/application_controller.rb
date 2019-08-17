c9c7bb20521a5cd6b484894e3883b0c69f5373a5
tomokoichikawa <takator@yahoo.co.jp>
2019-08-17 22:24:34 +0900

    Add:エラーページ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,21 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   before_action :login_required
 
+  protect_from_forgery with: :exception
+  include SessionsHelper
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from Exception, with: :render_500
+
+  def render_404
+    render file: "#{Rails.root}/public/404.html", status: 404, layout: false, content_type: 'text/html'
+  end
+
+  def render_500
+    render file: "#{Rails.root}/public/500.html", status: 500, layout: false, content_type: 'text/html'
+  end
+
   private
 
   def current_user
