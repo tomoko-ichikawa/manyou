ff90ec4e682df80e94a49a90caf8968215f9d540
tomokoichikawa <takator@yahoo.co.jp>
2019-08-17 13:10:37 +0900

    Fix:Rspec models/task_spec.rb

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -8,12 +8,13 @@ RSpec.describe Task, type: :model do
   end
 
   it "contentが空ならバリデーションが通らない" do
-    task = Task.new(task_name: '失敗しないテスト', content: '失敗テスト')
-    expect(task).to be_valid
+    task = Task.new(task_name: '失敗しないテスト', content: '')
+    expect(task).not_to be_valid
   end
 
   it "titleとcontentに内容が記載されていればバリデーションが通る" do
-    task = Task.new(task_name: '失敗しないテスト', content: '失敗しないテスト')
+    user = FactoryBot.create(:user)
+    task = Task.new(task_name: '失敗しないテスト', content: '失敗しないテスト', user_id: 1)
     expect(task).to be_valid
   end
 end
