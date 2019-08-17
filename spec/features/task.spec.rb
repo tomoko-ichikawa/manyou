13fbbe0d39ce8b0549582f4934b01355ae5e9953
tomokoichikawa <takator@yahoo.co.jp>
2019-08-17 11:41:06 +0900

    Fix:Rspec task.spec.rb

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -4,30 +4,32 @@ require 'rails_helper'
 RSpec.feature "タスク管理機能", type: :feature do
   # scenario（itのalias）の中に、確認したい各項目のテストの処理を書きます。
   background do
-    user = FactoryBot.create(:user)
-    FactoryBot.create(:task, user_id: user.id)
-    FactoryBot.create(:second_task, user_id: user.id)
-    FactoryBot.create(:third_task, user_id: user.id)
+    @user_1 = FactoryBot.create(:user)
+    @user_2 = FactoryBot.create(:admin_user)
 
+    FactoryBot.create(:task, user: @user_2)
+    FactoryBot.create(:second_task, user: @user_2)
+    FactoryBot.create(:third_task, user: @user_2)
+  end
+
+  def login_as_yohei
     visit new_session_path
-    fill_in 'Email', with: 'test@test.com'
-    fill_in 'Password', with: 'password'
+    fill_in "session[email]", with: @user_2.email
+    fill_in "session[password]", with: "password"
     find("#login").click
   end
 
   scenario "タスク一覧のテスト" do
-# あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
-    # tasks_pathにvisitする（タスク一覧ページに遷移する）
+    login_as_yohei
     visit tasks_path
-    save_and_open_page
-    # visitした（到着した）expect(page)に（タスク一覧ページに）「testtesttest」「samplesample」という文字列が
-    # have_contentされているか？（含まれているか？）ということをexpectする（確認・期待する）テストを書いている
+
     expect(page).to have_content 'Factoryで作ったデフォルトのタイトル１'
     expect(page).to have_content 'Factoryで作ったデフォルトのコンテント１'
 
   end
 
   scenario "タスク作成のテスト" do
+    login_as_yohei
     visit new_task_path
     fill_in 'task[task_name]', with: 'Factoryで作ったデフォルトのタイトル１'
     fill_in 'task[content]', with: 'Factoryで作ったデフォルトのコンテント１'
@@ -37,34 +39,41 @@ RSpec.feature "タスク管理機能", type: :feature do
   end
 
   scenario "タスク詳細のテスト" do
+    login_as_yohei
     visit tasks_path
     expect(page).to have_content 'Factoryで作ったデフォルトのタイトル１'
     expect(page).to have_content 'Factoryで作ったデフォルトのコンテント１'
   end
 
   scenario "タスクが作成日時の降順に並んでいるかのテスト" do
+     login_as_yohei
      visit tasks_path
      expect(Task.order("updated_at DESC").map(&:id))
   end
 
   scenario "タスクが終了期限の降順に並んでいるかのテスト" do
+    login_as_yohei
     visit tasks_path(sort_expired: "true")
+    expect(Task.order("updated_at DESC").map(&:id))
   end
 
   scenario "viewにてタスクが絞り込めるかのテスト" do
+    login_as_yohei
     visit tasks_path
     fill_in 'タスク名を検索', with: 'タイトル２'
     expect(page).to have_content 'タイトル２'
   end
 
   scenario "優先度が登録できるか" do
+    login_as_yohei
     visit tasks_path(sort_priority: "true")
     expect(page).to have_content '高'
   end
 
   scenario "優先度順にソートできているか" do
-   visit tasks_path
-   click_on '優先度でソートする'
-   expect(Task.order("priority ASC").map(&:id))
+    login_as_yohei
+    visit tasks_path
+    click_on '優先度でソートする'
+    expect(Task.order("priority ASC").map(&:id))
  end
 end
