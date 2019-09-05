5f998922e01fe1f6feb37f21a259b97b52969320
tomokoichikawa <takator@yahoo.co.jp>
2019-09-05 22:56:16 +0900

    Add:groupモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_025446) do
+ActiveRecord::Schema.define(version: 2019_09_05_135053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "group_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "labels", force: :cascade do |t|
     t.string "label_name"
@@ -40,6 +46,13 @@ ActiveRecord::Schema.define(version: 2019_08_20_025446) do
     t.bigint "user_id"
     t.index ["task_name"], name: "index_tasks_on_task_name"
     t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
+  create_table "user_groups", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
