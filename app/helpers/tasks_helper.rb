4fdd9e80adc9007c3e4aeefaec8a936eb19116d4
tomokoichikawa <takator@yahoo.co.jp>
2019-11-11 10:28:24 +0900

    Fix:添付画像

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,7 +1,7 @@
 module TasksHelper
   def choose_new_or_edit
     if action_name == 'new' || action_name == 'confirm'
-      confirm_tasks_path
+      tasks_path
     elsif action_name == 'edit'
       task_path
     end
